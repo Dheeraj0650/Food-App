9bd982ebde0bbbdbb55e464a038a329cc2ab5322
Kotte Dheeru <kottedheeru@Kottes-MacBook-Pro-16.local>
2024-04-14 21:34:09 -0600

    creating header component

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+export default Header = () => {
+  return (
+    <div className="Header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://t3.ftcdn.net/jpg/02/41/30/72/360_F_241307210_MjjaJC3SJy2zJZ6B7bKGMRsKQbdwRSze.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul className="font-bold">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
